f5fb799b03df544140e831747f7f1a0d87e9f5a5
Bernhard Janetzki <boerni@gmail.com>
2020-04-11 22:19:28 +0200

    module support, basic redash as example module - WIP

--- modules/__init__.py
+++ modules/__init__.py
@@ -10,37 +10,36 @@ class Modules(dict):
         self.cfg = cfg
 
     def _populate_cache(self):
-        if not self._cache:
-            # read and cache module info
-            modules_path = Path("modules/")
-            for module_path in [p for p in modules_path.iterdir() if p.is_dir()]:
-                # e.g. modules/spark_cluster => spark_cluster
-                module_info = {}
-                module_name = module_path.name
+        # read and cache module info
+        modules_path = Path("modules/")
+        for module_path in [p for p in modules_path.iterdir() if p.is_dir()]:
+            # e.g. modules/spark_cluster => spark_cluster
+            module_info = {}
+            module_name = module_path.name
 
-                # read module infos from modules __init__.py
-                exec(Path(module_path, "__init__.py").read_text(), module_info)
+            # read module infos from modules __init__.py
+            exec(Path(module_path, "__init__.py").read_text(), module_info)
+            self._cache[module_name] = {
+                k: module_info[k] for k in module_info.keys() & {"NAME", "DESCRIPTION", "WARNING"}
+            }
+            self._cache[module_name]["PATH"] = module_path.absolute()
+
+            # read module env
+            env = {"MODULE_ENV": {module_name: {}}}
+            exec(Path(module_path, "env.py").read_text(), env["MODULE_ENV"][module_name])
+            stack = env.copy()
+            exec(Path(module_path, "stack.py").read_text(), stack)
+            self._cache[module_name]["stack"] = stack
+
+            # read pre/post deploy code
+            pre_post_actions_path = Path(module_path, "pre_post_actions.py")
+            if pre_post_actions_path.exists():
+                pre_post_code = env.copy()
+                exec(pre_post_actions_path.read_text(), pre_post_code)
                 self._cache[module_name] = {
-                    k: module_info[k] for k in module_info.keys() & {"NAME", "DESCRIPTION", "WARNING"}
+                    **self._cache[module_name],
+                    **{k: pre_post_code[k] for k in pre_post_code.keys() & {"pre_deploy", "pre_destroy"}},
                 }
-                self._cache[module_name]["PATH"] = module_path.absolute()
-
-                # read module env
-                env = {"MODULE_ENV": {module_name: {}}}
-                exec(Path(module_path, "env.py").read_text(), env["MODULE_ENV"][module_name])
-                stack = env.copy()
-                exec(Path(module_path, "stack.py").read_text(), stack)
-                self._cache[module_name]["stack"] = stack
-
-                # read pre/post deploy code
-                pre_post_actions_path = Path(module_path, "pre_post_actions.py")
-                if pre_post_actions_path.exists():
-                    pre_post_code = env.copy()
-                    exec(pre_post_actions_path.read_text(), pre_post_code)
-                    self._cache[module_name] = {
-                        **self._cache[module_name],
-                        **{k: pre_post_code[k] for k in pre_post_code.keys() & {"pre_deploy", "pre_destroy"}},
-                    }
 
     def __setitem__(self, key, value):
         raise RuntimeError()
